9263853e702135c9e9207629df3239de74b7c159
Sander Steeghs <sander.steeghs@student.fontys.nl>
2017-01-09 13:04:18 +0100

    changed ISensor

--- MeasurementUnit/lib/include/SmartBeeHive/ISensor.hpp
+++ MeasurementUnit/lib/include/SmartBeeHive/ISensor.hpp
@@ -5,12 +5,15 @@
 
 class ISensor {
 public:
-    ISensor(IMeasurementSink &sink, int targetInterval, char* keyword) { }
+    ISensor(IMeasurementSink &sink, int targetInterval, char* type, int index) { }
     virtual ~ISensor() { }
 
     virtual void run() = 0;
-    virtual char* getKeyword() = 0;
-    virtual void setKeyword(char* keyword) = 0;
+    virtual char* getType() = 0;
+    virtual void setType(char* keyword) = 0;
+
+    virtual int getIndex() = 0;
+    virtual void setIndex(int index) = 0;
 };
 
 #endif // ISENSOR_HPP_
