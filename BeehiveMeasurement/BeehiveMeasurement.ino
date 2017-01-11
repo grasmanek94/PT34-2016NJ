f6916273c2a4e5d3e6405bc1cf4e272a88237fef
Unknown <ian_seelen@hotmail.com>
2017-01-11 12:44:28 +0100

    Fixed names

--- BeehiveMeasurement/BeehiveMeasurement.ino
+++ BeehiveMeasurement/BeehiveMeasurement.ino
@@ -13,11 +13,11 @@ static const uint8_t debugPin = 13;
 
 SerialSender serialSender(0);
 //DummySensor dummySensor(serialSender, interval, "12", 132);
-LightSensor lightSensor(serialSender, interval, "lux", 1);
-WeightSensor weightSensor(serialSender, interval, "Weight", 1, 3, 4);
-HumiditySensor humiditySensor(serialSender, interval, "Humidity", 0);
-TemperatureSensor temperatureSensor(serialSender, interval, "Temperature", 0);
-PressureSensor pressureSensor(serialSender, interval, "Pressure", 0);
+LightSensor lightSensor(serialSender, interval, "light-intensity", 1);
+WeightSensor weightSensor(serialSender, interval, "weight", 1, 3, 4);
+HumiditySensor humiditySensor(serialSender, interval, "humidity", 0);
+TemperatureSensor temperatureSensor(serialSender, interval, "temperature", 0);
+PressureSensor pressureSensor(serialSender, interval, "pressure", 0);
 
 void setup()
 {
